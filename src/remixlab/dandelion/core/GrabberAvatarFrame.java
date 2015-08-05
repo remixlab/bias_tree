495383e974ac92bb9b415bd6b16a9f8cb4d6e119
Jean Pierre Charalambos <nakednous@gmail.com>
2015-08-05 18:05:22 -0500

    GrabberAvatarFrame api docs completed

--- src/remixlab/dandelion/core/GrabberAvatarFrame.java
+++ src/remixlab/dandelion/core/GrabberAvatarFrame.java
@@ -1,3 +1,13 @@
+/*********************************************************************************
+ * dandelion_tree
+ * Copyright (c) 2014 National University of Colombia, https://github.com/remixlab
+ * @author Jean Pierre Charalambos, http://otrolado.info/
+ *
+ * All rights reserved. Library that eases the creation of interactive
+ * scenes, released under the terms of the GNU Public License v3.0
+ * which is available at http://www.gnu.org/licenses/gpl.html
+ *********************************************************************************/
+
 package remixlab.dandelion.core;
 
 import remixlab.bias.core.Agent;
@@ -5,6 +15,15 @@ import remixlab.bias.event.*;
 import remixlab.dandelion.geom.*;
 import remixlab.util.*;
 
+/**
+ * The GrabberAvatarFrame class represents a GrabberFrame that can be tracked by an Eye, i.e., it implements
+ * the Trackable interface.
+ * <p>
+ * The {@link #eyeFrame()} of the camera that is to be tracking the frame (see the documentation of the Trackable
+ * interface) is defined in spherical coordinates ({@link #azimuth()}, {@link #inclination()} and
+ * {@link #trackingDistance()}) respect to the {@link #position()} (which defines the
+ * {@link remixlab.dandelion.core.Eye#at()} Vec).
+ */
 public class GrabberAvatarFrame extends GrabberFrame implements Trackable, Copyable {
 	@Override
 	public int hashCode() {
